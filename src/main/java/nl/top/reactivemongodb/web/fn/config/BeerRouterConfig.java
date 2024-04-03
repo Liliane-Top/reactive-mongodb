ee467a4cb8ce6df5eb3c621e76cf665e92fadd82
Liliane Top <hallo@lilianetop.nl>
2024-04-03 14:34:08 +0200

    removed the generic abtract class GenericRouterConfig and now back to duplicated code but all tests pass

--- src/main/java/nl/top/reactivemongodb/web/fn/config/BeerRouterConfig.java
+++ src/main/java/nl/top/reactivemongodb/web/fn/config/BeerRouterConfig.java
@@ -1,17 +1,33 @@
 package nl.top.reactivemongodb.web.fn.config;
 
+import lombok.RequiredArgsConstructor;
 import nl.top.reactivemongodb.web.fn.handlers.BeerHandler;
-import nl.top.reactivemongodb.web.fn.handlers.ResourceHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
-public class BeerRouterConfig extends GenericRouterConfig<ResourceHandler<BeerHandler>> {
+@RequiredArgsConstructor
+public class BeerRouterConfig {
 
+    private final BeerHandler handler;
     public static final String BEER_PATH = "/api/v3/beer";
     public static final String BEER_PATH_ID = BEER_PATH + "/{beerId}";
-
-    public BeerRouterConfig(BeerHandler handler) {
-        super(BEER_PATH, BEER_PATH_ID, handler);
+    @Bean
+    public RouterFunction<ServerResponse> beerRoutes() {
+        return route()
+                .GET(BEER_PATH, accept(APPLICATION_JSON), handler::getList)
+                .GET(BEER_PATH_ID, accept(APPLICATION_JSON), handler::getById)
+                .POST(BEER_PATH, accept(APPLICATION_JSON), handler::create)
+                .PUT(BEER_PATH_ID, accept(APPLICATION_JSON), handler::updateById)
+                .PATCH(BEER_PATH_ID, accept(APPLICATION_JSON), handler::patchById)
+                .DELETE(BEER_PATH_ID, accept(APPLICATION_JSON), handler::deleteById)
+                .build();
     }
 
 }
