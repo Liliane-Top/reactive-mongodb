327afe6ef4fe89d3010b112e0e74503084e4f13e
Liliane Top <hallo@lilianetop.nl>
2024-04-17 15:27:07 +0200

    added commandlinerunner to print all created beans

--- src/main/java/nl/top/reactivemongodb/ReactiveMongoDbApplication.java
+++ src/main/java/nl/top/reactivemongodb/ReactiveMongoDbApplication.java
@@ -1,13 +1,31 @@
 package nl.top.reactivemongodb;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class ReactiveMongoDbApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReactiveMongoDbApplication.class, args);
+    }
+    @Bean
+    public ApplicationRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+        };
     }
 
 }
