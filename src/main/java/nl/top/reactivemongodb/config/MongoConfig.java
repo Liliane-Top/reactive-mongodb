9f0c499e460431349ba99f1b4efef607c126d149
Liliane Top <hallo@lilianetop.nl>
2024-05-21 15:45:42 +0200

    docker-compose file requires an image of the authorization service to be able to check if the the token is valid, otherwise error 500 and no access to the mongodb

--- src/main/java/nl/top/reactivemongodb/config/MongoConfig.java
+++ src/main/java/nl/top/reactivemongodb/config/MongoConfig.java
@@ -1,10 +1,15 @@
 package nl.top.reactivemongodb.config;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+
+import static java.util.Collections.singletonList;
 
 @Configuration
 public class MongoConfig extends AbstractReactiveMongoConfiguration {
@@ -21,13 +26,13 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
     }
 
     //this is required if you use a Docker image in which you have set up client credentials
-//    @Override
-//    protected void configureClientSettings(MongoClientSettings.Builder builder) {
-//        builder.credential(MongoCredential.createCredential("root",
-//                        "admin", "example".toCharArray()))
-//                .applyToClusterSettings(settings ->
-//                        settings.hosts((singletonList(new ServerAddress("127.0.0.1", 27017)))));
-//    }
+    @Override
+    protected void configureClientSettings(MongoClientSettings.Builder builder) {
+        builder.credential(MongoCredential.createCredential("root",
+                        "admin", "example".toCharArray()))
+                .applyToClusterSettings(settings ->
+                        settings.hosts((singletonList(new ServerAddress("mongo", 27017)))));
+    }
 
 
 }
